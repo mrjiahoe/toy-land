a49e372c780a3ec9c7e9afd5f41c5f197d1b60f1
jiahoe <mrjiahoe@hotmail.com>
2021-08-13 18:01:03 +0800

    changed font

--- src/globalStyles.js
+++ src/globalStyles.js
@@ -5,7 +5,7 @@ const GlobalStyle = createGlobalStyle`
         box-sizing: border-box;
         margin: 0;
         padding: 0;
-        font-family:  sans-serif;
+        font-family: 'Lato', sans-serif;
     }
 `;
 
